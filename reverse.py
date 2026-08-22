1389f1771b02b2554bb0341c5f345d831455a774
yash <yashwadve648@gmail.com>
2026-08-22 12:27:46 +0530

    Solve Reverse String problem in Python

---
+++ reverse.py
@@ -0,0 +1,11 @@
+# reverse the string
+string = "yashwadve"
+
+reversed_string = string[::-1]
+print(reversed_string)
+
+# reverse the number
+number = 12345
+number_string = str(number)
+reversed_number = int(number_string[::-1])
+print(reversed_number)
